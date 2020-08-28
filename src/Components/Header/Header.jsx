bd78e92c08deae0a2be81f6f1ddfac234f74340d
webmarksdev <karenmarkswebdev@gmail.com>
2020-08-27 22:19:07 -0400

    Starter files with boostrap, nav, base structure

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
+import PKLogo from '../../Assets/Images/pklogo.jpg';
+
+
+class Header extends Component {
+
+    render() {
+        return (
+            <Navbar bg="light" expand="lg">
+                <Navbar.Brand href="#home">
+                    <img src={PKLogo} alt="Perryman Keglovits Logo"></img>    
+                    Perryman & Keglovits Foundation
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="mr-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#link">About</Nav.Link>
+                        <Nav.Link href="#link">Contact</Nav.Link>
+                        <Nav.Link href="#link">Archives</Nav.Link>
+                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                            <NavDropdown.Item href="#action/3.1">Press Releases</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.2">Spirit Awards</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.3">Photo Gallery</NavDropdown.Item>
+                            <NavDropdown.Item href="#action/3.4">Video Highlights</NavDropdown.Item>
+                        </NavDropdown>
+                    </Nav>
+                {/* <Form inline>
+                    <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+                    <Button variant="outline-success">Search</Button>
+                </Form> */}
+                </Navbar.Collapse>
+            </Navbar>
+        )
+    }
+}
+
+export default Header;
