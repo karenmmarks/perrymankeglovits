7cea8f40231e2c0e23ed074a1120b3d0364eb40d
webmarksdev <karenmarkswebdev@gmail.com>
2020-09-17 23:21:53 -0400

    Fixed Header, moved index.html in src dir

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 import { Navbar, Nav, NavDropdown } from 'react-bootstrap';
 import PKLogo from '../../Assets/Images/pklogo.jpg';
 
-
+import './Header.css';
 class Header extends Component {
 
     render() {
         return (
             <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">
-                    <img src={PKLogo} alt="Perryman Keglovits Logo"></img>    
-                    Perryman & Keglovits Foundation
+                <Navbar.Brand href="#home" width= "100px">
+                 <img src={PKLogo} height="100px" alt="Perryman Keglovits Logo" className="p-1"></img>    
+                <span className="d-flex float-right text-info font-weight-bold">Perryman <br></br> & Keglovits <br></br>Foundation</span>
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
@@ -19,8 +19,7 @@ class Header extends Component {
                         <Nav.Link href="#home">Home</Nav.Link>
                         <Nav.Link href="#link">About</Nav.Link>
                         <Nav.Link href="#link">Contact</Nav.Link>
-                        <Nav.Link href="#link">Archives</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                        <NavDropdown title="Archives" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Press Releases</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.2">Spirit Awards</NavDropdown.Item>
                             <NavDropdown.Item href="#action/3.3">Photo Gallery</NavDropdown.Item>
